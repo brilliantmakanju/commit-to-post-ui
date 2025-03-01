b5a009395d1188ec31a3cdc0bd9b05bb76b9b4a3
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-03-01 23:36:15 +0000

    chore: trying to fix error page.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	i18n: {
-		locales: ["en-US", "fr", "de"], // Supported locales
-		defaultLocale: "en-US", // Default locale
+		locales: ["en-US", "fr", "de"],
+		defaultLocale: "en-US",
 	},
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: "/:path*", // Catch all unmatched routes
+				destination: "/not-found", // Your custom 404 page
+				permanent: false,
+				missing: [
+					{
+						type: "query",
+						key: "path", // Providing the required 'key' property
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
