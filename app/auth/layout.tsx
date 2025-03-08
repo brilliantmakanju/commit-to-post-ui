2c16ca08129895310bfc8d7b29fc730302ffae65
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-03-08 15:03:34 +0000

    chore: SEO optimization for auth page, inbox page.

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,26 @@
 import "@/app/globals.css";
 
+import { Metadata } from "next";
 import { Suspense } from "react";
 
 import { LogoutModal } from "@/components/auth/modals/logout-modal";
 import { Toaster } from "@/components/ui/sonner";
+
+export const metadata: Metadata = {
+	metadataBase: new URL("https://commit.jolexhive.com/"),
+	title: "Push to Post - Seamless Access",
+	description:
+		"Access Push to Post securely and effortlessly. Log in, sign up, reset your password, or generate a magic link to get started with your AI-powered content assistant.",
+	openGraph: {
+		title: "Push to Post - Seamless Access",
+		description:
+			"Access Push to Post securely and effortlessly. Log in, sign up, reset your password, or generate a magic link to get started with your AI-powered content assistant.",
+		type: "website",
+		url: "https://commit.jolexhive.com",
+		siteName: "Push to Post",
+		locale: "en_US",
+	},
+};
 
 export default function RootLayout({
 	children,
--- app/inbox/[uid]/[token]/layout.tsx
+++ app/inbox/[uid]/[token]/layout.tsx
@@ -1,10 +1,26 @@
 import "@/app/globals.css";
 
+import { Metadata } from "next";
 import { Suspense } from "react";
 
 import { LogoutModal } from "@/components/auth/modals/logout-modal";
 import AuthTopNavigation from "@/components/navigation/top_navigation/auth-top-navigation";
 import { Toaster } from "@/components/ui/sonner";
+export const metadata: Metadata = {
+	metadataBase: new URL("https://commit.jolexhive.com/"),
+	title: "Push to Post - Verify Your Email",
+	description:
+		"Complete your Push to Post setup by verifying your email. Secure your account and unlock seamless access to your AI-powered content assistant.",
+	openGraph: {
+		title: "Push to Post - Verify Your Email",
+		description:
+			"Complete your Push to Post setup by verifying your email. Secure your account and unlock seamless access to your AI-powered content assistant.",
+		type: "website",
+		url: "https://commit.jolexhive.com/verify-email",
+		siteName: "Push to Post",
+		locale: "en_US",
+	},
+};
 
 export default function RootLayout({
 	children,
