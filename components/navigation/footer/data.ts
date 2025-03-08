f68f6de66fd2e56a407ef3a8c2f88b2833b1ef8a
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-03-08 23:29:57 +0000

    chore: added refund policy page to footer

--- components/navigation/footer/data.ts
+++ components/navigation/footer/data.ts
@@ -50,6 +50,8 @@ export const footerData: FooterData = {
 	legalLinks: [
 		{ label: "Privacy Policy", href: "/privacy" },
 		{ label: "Terms of Service", href: "/terms" },
+		{ label: "Refund Policy", href: "/refund-policy" },
+
 		// { label: "Cookie Policy", href: "/cookies" },
 	],
 	copyright: "© 2025 Push to Post. All rights reserved.",
