6ee90b0fee5217a0e818fce0202c770e9a9d2b99
Jol3x D3v <brilliantmakanju4@gmail.com>
2026-01-23 16:21:02 +0100

    chore: updated middleware

--- middleware.ts
+++ middleware.ts
@@ -65,7 +65,8 @@ export default auth(async request => {
 
 export const config = {
 	matcher: [
-		String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
+		// eslint-disable-next-line quotes
+		`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
 		"/(api|trpc)(.*)",
 	],
 };
